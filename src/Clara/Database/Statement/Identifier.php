5892a9d349edd1ad13b3786dbe210ecb78b2e5fe
Kevin O'Rourke <kevin@korourke.net>
2013-10-30 16:08:06 -0700

    Identifier (mysql type) class + tests

---
+++ src/Clara/Database/Statement/Identifier.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Identifier.php
+ *
+ * This DocBlock was generated automatically by PhpStorm
+ *
+ * @author      Kevin O'Rourke <kevin@korourke.net>
+ * @package     Clara
+ */
+
+namespace Clara\Database\Statement;
+
+use Clara\Database\Statement\Exception\StatementException;
+
+
+/**
+ * This class represents a MySQL identifier (table name, column name, etc).
+ * See @link for information on restrictions and best practices
+ *
+ * @package Clara\Database\Statement
+ * @link http://dev.mysql.com/doc/refman/5.5/en/identifiers.html
+ */
+class Identifier {
+
+	/**
+	 * @var
+	 */
+	protected $name;
+
+	/**
+	 * @var string
+	 */
+	protected $prefix;
+
+	/**
+	 * @var string
+	 */
+	protected $alias;
+
+	/**
+	 * @param        $name
+	 * @param string $alias
+	 * @param string $prefix
+	 */
+	public function __construct($name, $alias='', $prefix='') {
+		self::validate($name);
+		$this->name = $name;
+
+		if( ! empty($alias)) {
+			self::validate($alias);
+		}
+		$this->alias = $alias;
+
+		if( ! empty($prefix)) {
+			self::validate($prefix, true);
+		}
+		$this->prefix = $prefix;
+	}
+
+	/**
+	 * @param string $alias
+	 * @return $this
+	 */
+	public function setAlias($alias) {
+		$this->alias = $alias;
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getAlias() {
+		return $this->alias;
+	}
+
+	/**
+	 * @param mixed $name
+	 * @return $this
+	 */
+	public function setName($name) {
+		$this->name = $name;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * @param string $prefix
+	 * @return $this
+	 */
+	public function setPrefix($prefix) {
+		$this->prefix = $prefix;
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPrefix() {
+		return $this->prefix;
+	}
+
+	/**
+	 * @param      $str
+	 * @param bool $isAlias
+	 * @return bool
+	 * @throws \Clara\Database\Statement\Exception\StatementException
+	 */
+	public static function validate($str, $isAlias=false) {
+		if(self::isValid($str, $isAlias)) {
+			return true;
+		}
+		throw new StatementException('Invalid mysql identifier provided. Expecting: string. Received: ' . gettype($str) . '.');
+
+	}
+
+	/**
+	 * @return bool
+	 */
+	public static function isValid($str, $isAlias) {
+		if( ! empty($str) && is_string($str) && preg_match('#^[0-9,a-z,A-Z$_]+$#', $str)) {
+			$maxLength = ($isAlias) ? 256 : 64;
+			if(strlen($str) <= $maxLength) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function __toString() {
+		$str = '';
+		if($this->prefix) {
+			$str .= sprintf('`%s`.', $this->prefix);
+		}
+		$str .= sprintf('`%s`', $this->name);
+		if($this->alias) {
+			$str .= sprintf(' AS `%s`', $this->alias);
+		}
+		return $str;
+	}
+
+} 
---
+++ tests/Database/Statement/IdentifierTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * IdentifierTest.php
+ *
+ * This DocBlock was generated automatically by PhpStorm
+ *
+ * @author      Kevin O'Rourke <kevin@korourke.net>
+ * @package     Clara
+ */
+
+use Clara\Database\Statement\Identifier;
+
+class IdentifierTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @covers Clara\Database\Statement\Identifier::__construct
+	 */
+	public function testBasicCreate() {
+		$id = new Identifier('foo', 'alias', 'prefix');
+		$this->assertAttributeSame('foo', 'name', $id);
+		$this->assertAttributeSame('alias', 'alias', $id);
+		$this->assertAttributeSame('prefix', 'prefix', $id);
+	}
+
+	public function provideInvalidNames() {
+		return array(
+			array(''),
+			array(null),
+			array(1234),
+			array(3.14159),
+			array(false),
+			array(true),
+			array(new StdClass),
+		);
+	}
+
+	/**
+	 * @dataProvider provideInvalidNames
+	 * @expectedException \Clara\Database\Statement\Exception\StatementException
+	 */
+	public function testInvalidNamesThrowException($name) {
+		$id = new Identifier($name);
+	}
+
+	public function provideIdentifiersAndExpectedStrings() {
+		return array(
+			array('foo', '', '', '`foo`'),
+			array('bar', 'barAlias', '', '`bar` AS `barAlias`'),
+			array('baz', 'bazAlias', 'bazPrefix', '`bazPrefix`.`baz` AS `bazAlias`'),
+		);
+	}
+
+	/**
+	 * @covers Clara\Database\Statement\Identifier::__toString
+	 * @dataProvider provideIdentifiersAndExpectedStrings
+	 */
+	public function testToString($name, $alias, $prefix, $expected) {
+		$id = new Identifier($name, $alias, $prefix);
+		$this->assertSame($expected, (string)$id);
+	}
+
+	public function provideIdentifierNamesForTesting() {
+		return array(
+			array('', false, false),
+			array('1234567890123456789012345678901234567890123456789012345678901234567890', false, false), //over 64 chars
+			array('1234567890', false, true),
+			array('abc$_123', false, true),
+			array(str_repeat('a', 257), true, false),
+			array(str_repeat('a', 255), true, true),
+		);
+	}
+
+	/**
+	 * @covers Clara\Database\Statement\Identifier::isValid
+	 * @dataProvider provideIdentifierNamesForTesting
+	 */
+	public function testStaticIdentifierValidation($name, $isAlias, $expected) {
+		$this->assertSame($expected, Identifier::isValid($name, $isAlias));
+	}
+
+}
+ 
