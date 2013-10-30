30ed41f2f67052e457291909ac95a94356d90528
Kevin O'Rourke <kevin@korourke.net>
2013-10-30 16:47:00 -0700

    database and statement exceptions added

---
+++ src/Clara/Database/Exception/DatabaseException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * DatabaseException.php
+ *
+ * This DocBlock was generated automatically by PhpStorm
+ *
+ * @author      Kevin O'Rourke <kevin@korourke.net>
+ * @package     Clara
+ */
+
+namespace Clara\Database\Exception;
+
+use Clara\Exception\ClaraRuntimeException;
+
+
+class DatabaseException extends ClaraRuntimeException {
+
+} 
---
+++ src/Clara/Database/Statement/Exception/StatementException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * StatementException.php
+ *
+ * This DocBlock was generated automatically by PhpStorm
+ *
+ * @author      Kevin O'Rourke <kevin@korourke.net>
+ * @package     Clara
+ */
+
+namespace Clara\Database\Statement\Exception;
+
+use Clara\Database\Exception\DatabaseException;
+
+
+class StatementException extends DatabaseException {
+
+}
