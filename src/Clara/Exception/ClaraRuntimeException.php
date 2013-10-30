5ea0fa95b28241ed6e6d020d89b2bfa071a09635
Kevin O'Rourke <kevin@korourke.net>
2013-10-30 16:05:15 -0700

    new base exception type: ClaraRuntimeException

---
+++ src/Clara/Exception/ClaraRuntimeException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * ClaraRuntimeException.php
+ *
+ * This DocBlock was generated automatically by PhpStorm
+ *
+ * @author      Kevin O'Rourke <kevin@korourke.net>
+ * @package     Clara
+ */
+
+namespace Clara\Exception;
+
+use RuntimeException;
+
+
+class ClaraRuntimeException extends RuntimeException {
+
+} 
