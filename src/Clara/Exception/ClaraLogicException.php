c8f69f4cb08e2b3e4c5e6e4ac691edb47915c008
Kevin O'Rourke <kevin@spurkle.com>
2013-11-06 15:19:05 -0800

    new exception type: ClaraLogicException

---
+++ src/Clara/Exception/ClaraLogicException.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * ClaraLogicException.php
+ *
+ * This DocBlock was generated automatically by PhpStorm
+ *
+ * @author      Kevin O'Rourke <kevin@korourke.net>
+ * @package     Clara
+ */
+
+namespace Clara\Exception;
+
+use \Exception;
+use \LogicException;
+
+
+class ClaraLogicException extends LogicException {
+
+	/**
+	 * @var Exception
+	 */
+	public $previous;
+
+	/**
+	 * @param Exception $e
+	 * @return $this
+	 */
+	public function setPrevious(Exception $e) {
+		$this->previous = $e;
+		return $this;
+	}
+
+}
