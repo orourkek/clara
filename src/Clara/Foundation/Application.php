e60de48021469eda2c5131edabbf018580765a80
Kevin O'Rourke <kevin@spurkle.com>
2013-11-15 12:53:09 -0800

    added Application and ApplicationConfig classes

---
+++ src/Clara/Foundation/Application.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Application.php
+ *
+ * This DocBlock was generated automatically by PhpStorm
+ *
+ * @author      Kevin O'Rourke <kevin@korourke.net>
+ * @package     Clara
+ */
+
+namespace Clara\Foundation;
+
+use Clara\Events\Observable;
+use Clara\Events\Event;
+use Clara\Http\Request;
+use Clara\Http\Response;
+use Clara\Routing\Exception\RoutingException;
+use Clara\Routing\Router;
+
+/**
+ * Class Application
+ *
+ * @package Clara\Foundation
+ */
+class Application extends Observable {
+
+	/**
+	 * @var \Clara\Routing\Router
+	 */
+	protected $router;
+
+	/**
+	 * @var \Clara\Foundation\ApplicationConfig
+	 */
+	protected $config;
+
+	/**
+	 * @param \Clara\Foundation\ApplicationConfig $config
+	 */
+	public function __construct(ApplicationConfig $config) {
+		$this->config = $config;
+		$this->router = new Router();
+		try {
+			$this->router->importRoutesFromFile($this->config['routesFile']);
+			$this->fire(new Event('application.routes-loaded', $this, $this->config['routesFile']));
+		} catch(RoutingException $e) {}
+		$this->fire(new Event('application.created', $this));
+	}
+
+	/**
+	 * @param \Clara\Http\Request $request
+	 * @uses \Clara\Routing\Route::run
+	 */
+	public function run(Request $request) {
+		$this->fire(new Event('application.run', $this, $request));
+		if($matchedRoute = $this->router->route($request)) {
+			$this->fire(new Event('application.run.routed', $this, $matchedRoute));
+			$response = $matchedRoute->run();
+			$this->fire(new Event('application.run.response', $this, $response));
+			$response->send();
+		} else {
+			$this->fire(new Event('application.run.not-found', $this, $response));
+			$response = new Response('404 Not Found', Response::HTTP_NOT_FOUND);
+			$response->send();
+		}
+		$this->fire(new Event('application.run.complete', $this, $request));
+	}
+
+}
---
+++ src/Clara/Foundation/ApplicationConfig.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * ApplicationConfig.php
+ *
+ * This DocBlock was generated automatically by PhpStorm
+ *
+ * @author      Kevin O'Rourke <kevin@korourke.net>
+ * @package     Clara
+ */
+
+namespace Clara\Foundation;
+
+use Clara\Support\Collection;
+
+/**
+ * Hold various config values for the Application. Can be expanded/extended to add further configuration to the Application class.
+ *
+ * @package Clara\Foundation
+ */
+class ApplicationConfig extends Collection {
+
+	/**
+	 * Array of application config values
+	 *
+	 * @var array
+	 */
+	protected $items = array(
+
+		/**********************************************************************
+		 * Routes File
+		 **********************************************************************
+		 *
+		 * The location (FULL PATH) of a routes file to be loaded into the
+		 * application's router.
+		 *
+		 * See the routing README for information on route file formatting
+		 */
+		'routesFile' => '',
+
+	);
+
+	/**
+	 * Creates a new ApplicationConfig object by merging values found in a config file with defaults
+	 *
+	 * @param string $location
+	 * @return \Clara\Foundation\ApplicationConfig
+	 */
+	public static function fromFile($location) {
+		$config = new self();
+		$content = require $location;
+		if(is_array($content) || $content instanceof Collection) {
+			$config->merge($content);
+		} else {
+			trigger_error('Invalid application configuration file', E_USER_ERROR);
+		}
+		return $config;
+	}
+
+} 
