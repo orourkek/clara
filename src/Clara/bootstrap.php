9b2a48439669087588b1ac63ffe5c9f756559372
Kevin O'Rourke <kevin@spurkle.com>
2013-11-19 10:24:07 -0800

    new bootstrap file instead of simply including autoload.php

---
+++ src/Clara/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * bootstrap.php
+ *
+ * This DocBlock was generated automatically by PhpStorm
+ *
+ * @author      Kevin O'Rourke <kevin@korourke.net>
+ * @package     Clara
+ */
+
+require __DIR__ . '/constants.php';
+require __DIR__ . '/autoload.php';
---
+++ src/Clara/constants.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * constants.php
+ *
+ * This DocBlock was generated automatically by PhpStorm
+ *
+ * @author      Kevin O'Rourke <kevin@korourke.net>
+ * @package     Clara
+ */
+
+/**
+ * The root directory (.../src/Clara)
+ */
+define('CLARA_ROOT_DIR', __DIR__);
+
+/**
+ * Where external libraries can be found
+ */
+define('CLARA_LIB_DIR', dirname(dirname(__DIR__)) . '/lib');
